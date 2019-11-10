838a1d536de73f11e000182a8ce3a7a8e66cada0
Angela Edwards <edwards11914@outlook.com>
2019-11-10 15:42:33 -0500

    prep for constructor

--- Grounding.js
+++ Grounding.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+
+
+
+
+
+
+
+
+
+
+
+export default Grounding; 
--- Home.js
+++ Home.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import { homedir } from 'os';
+
+
+
+
+
+
+
+
+
+
+
+
+export default Home; 
