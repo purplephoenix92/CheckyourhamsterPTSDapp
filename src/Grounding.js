fe311cdafe5552fc05eebf44c365212976f59358
Angela Edwards <edwards11914@outlook.com>
2019-12-07 16:51:51 -0500

    trying to debug

--- src/Grounding.js
+++ src/Grounding.js
@@ -17,11 +17,17 @@ class Grounding extends React.Component {
                 Grounding is a useful tool when you don't really feel completely present in a room, are having a flashback, or your emotions are compelling
                 you to relive a difficult, traumatic memory. 
             <div>
+            <div> "" </div>
                 So ask yourself: </div>
+                <div> "" </div>
                <div> What are five things you can see? </div>
+               <div> ""</div>
             <div>What are four things you can hear? </div>
+            <div> "" </div>
             <div>What are three things you can touch? </div>
+            <div> ""</div>
             <div>What are two things you can smell? </div>
+            <div> ""</div>
             <div>What is one thing you can taste?</div>
             </h3>
             </div>
--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,7 @@ function Home() {
     return (
      <h2> So, why a Hamster? My therapist told me once that in my head sometimes is a raging hamster on a wheel: speedy, angry, and stuck. I hope this site allows you to feel less stuck. </h2>
     );}
-
-    function Header() {
+function TwoHamstersStrawberry() {
       // Import result is the URL of your image
       return <img src={logo} alt="Logo" />;
     }
